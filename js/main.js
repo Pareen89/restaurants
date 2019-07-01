af9bcc90772a3312424bece22888a9beada2f8cd
pareen89 <pareenpatel89@gmail.com>
2019-07-01 11:05:20 -0700

    Updated links and added slider images

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ function drop() {
   console.log('licked');
   document.getElementById('containerOne').classList.toggle('stop');
 }
+
+// window.addEventListener('load', slideshow());
+
+// function slideshow() {
+//   let slideIndex = 0;
+//   let images = document.getElementsByClassName('slideshow__images');
+//   for (let i = 0; i < images.length; i++) {
+//     images[i].style.display = 'none';
+//   }
+//   slideIndex++;
+//   if (slideIndex > images.length) {
+//     slideIndex = 1;
+//   }
+//   images[slideIndex - 1].style.display = 'block';
+//   setTimeout(slideshow, 3000);
+// }
